beb815f21c5a828c650937dd45ac05a0762484b8
simelane-jpd <201575227@student.uj.ac.za>
2022-03-18 13:43:59 +0200

    added mark tag on checkbox to highlight words more than 5 letters

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const longestWord = document.querySelector(".longest")
+
 const box = document.querySelector(".tick")
 const button = document.querySelector(".btn");
 const count = document.querySelector(".counter")
 const character = document.querySelector(".text");
 const show = document.querySelector(".sentence");
 
-let words = '';
+let wordWidget = '';
 
 
 
@@ -12,22 +14,27 @@ let words = '';
 let myFunc = () => {
     let func = character.value;
     let arr = func.split(' ')
-    let sent = ''
+    let go = ''
+   
     let counter = 0;
     for (let i = 0; i < arr.length; i++) {
         const array = arr[i];
         if (array.length >= 5) {
-            sent += `<mark style="background-color: #f72585;" >${array}</mark>`
+            go += `<mark style="background-color: #f72585;" >${array}</mark> `
         }
         else {
-            sent += array + ' '
+            go += array + ' '
             counter++
         }
     }
-    console.log(sent);
-    show.innerHTML = sent;
+
+ 
+    console.log(go);
+
+    show.innerHTML = go;
     count.innerHTML = "Word count: " + arr.length;
 }
+
 
 button.addEventListener('click', myFunc)
 
@@ -35,21 +42,29 @@ button.addEventListener('click', myFunc)
 let myFunc2 = () => {
     let func = character.value;
     let arr = func.split(' ');
-    let sent = ''
+    let go = ''
     if (box.checked == true) {
         for (let i = 0; i < arr.length; i++) {
             const array = arr[i]
             if (array.length <= 5) {
-                sent += `<span style="display: none">${array} </span>`
+                go += `<span style="display: none">${array} </span>`
+                
             } else {
-                sent += array + ' '
+                go += array + ' '
             }
         }
-        console.log(sent);
-        show.innerHTML = sent;
+        console.log(go);
+        show.innerHTML = `<mark style="background-color: #f72585;" >${go}</mark> `
     }
     else {
         myFunc()
     }
 }
 box.addEventListener('click', myFunc2)
+
+
+    
+
+
+
+
