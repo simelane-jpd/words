9df473969e41d43bc338b5c26350a379a433beff
simelane-jpd <201575227@student.uj.ac.za>
2022-03-22 16:16:28 +0200

    added longest word but still have to remove its button

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const longestWord = document.querySelector(".longest")
-
 const box = document.querySelector(".tick")
 const button = document.querySelector(".btn");
 const count = document.querySelector(".counter")
 const character = document.querySelector(".text");
 const show = document.querySelector(".sentence");
+
 
 let wordWidget = '';
 
@@ -19,7 +19,7 @@ let myFunc = () => {
     let counter = 0;
     for (let i = 0; i < arr.length; i++) {
         const array = arr[i];
-        if (array.length >= 5) {
+        if (array.length >4) {
             go += `<mark style="background-color: #f72585;" >${array}</mark> `
         }
         else {
@@ -38,7 +38,7 @@ let myFunc = () => {
 
 button.addEventListener('click', myFunc)
 
-
+//checkbox to hide shorter words//
 let myFunc2 = () => {
     let func = character.value;
     let arr = func.split(' ');
@@ -63,8 +63,37 @@ let myFunc2 = () => {
 box.addEventListener('click', myFunc2)
 
 
-    
+let theLongestWord = ()=> {
+    let func = character.value;
+    let arr = func.split(/\s/);
+    let go = arr.sort( (a, b) => {
+        return b.length - a.length
 
+    })
+        console.log(go);
+        show.innerHTML = `<mark style="background-color:yellow;">${go[0]}</mark> `;
 
+}
 
+longestWord.addEventListener('click', theLongestWord)
 
+//let getTheLongestWord = () => {
+   // let func = character.value;
+    //let arr = func.split(' ');
+    //let go = ''
+    //const words = textInput.value;
+    //const word = words.split(" ");
+  
+    //let longestWord = "";
+  
+    //for (let i = 0; i < arr.length; i++) {
+        //if(arr[i].length >= go.length) {
+          //  go = arr[i]
+        //}
+   // }
+   // show.innerHTML = `<mark style="background-color: green;">${go}</mark>`;
+   // }
+
+   // longestWord.addEventListener('click', getTheLongestWord)
+
+  
